fe2a082be3be8474f8fa0066eaf8165549a4bc84
amorris0329 <andersonjmorris@icloud.com>
2025-01-28 17:17:26 -0500

    starting work on database

--- GrooveGuesser/admin.py
+++ GrooveGuesser/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Account
 
 # Register your models here.
+
+class AccountAdmin(admin.ModelAdmin):
+    fields = [
+        "username", "password",
+    ]
+
+admin.site.register(Account, AccountAdmin)
--- GrooveGuesser/models.py
+++ GrooveGuesser/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Account(models.Model):
+    username = models.CharField(max_length=15)
+    password = models.CharField(max_length=20)
+
+    def __str__(self):
+        return "User " + self.username
+    
+ 
