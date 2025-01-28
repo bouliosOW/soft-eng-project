9c68ea50766b02f5bc6c2ab641c582d42b5c9683
amorris0329 <andersonjmorris@icloud.com>
2025-01-28 17:30:57 -0500

    Added join date to Account model

--- GrooveGuesser/models.py
+++ GrooveGuesser/models.py
@@ -5,6 +5,7 @@ from django.db import models
 class Account(models.Model):
     username = models.CharField(max_length=15)
     password = models.CharField(max_length=20)
+    join_data = models.DateTimeField("Join Date")
 
     def __str__(self):
         return "User " + self.username
