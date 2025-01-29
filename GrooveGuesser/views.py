0dcceb170a4adfe667e7387163d8c3344d7bfc02
PatrickFratus <fratusp52@gmail.com>
2025-01-28 21:14:48 -0500

    I made a simple design for the website. Im also pretty sure I fixed the user class problem and made the user class. If you would like me to change any messages let me know.

--- GrooveGuesser/views.py
+++ GrooveGuesser/views.py
@@ -1,8 +1,21 @@
 from django.shortcuts import render
+# from django.contrib.auth.forms import UserCreationForm #registration (WIP PF)
 from django.http import HttpResponse,JsonResponse
 
+#grooveguesser test responses
 def index(request):
-    return HttpResponse("Hello, you're accessing the GrooveGuesser app.")
+    # return HttpResponse("Hello, Welcome to the GrooveGuesser app.") (WIP PF)
+    return render(request, 'GrooveGuesser.html')
 
 def about(request):
-    return HttpResponse("GrooveGuesser is a song guessing game.")
+    # return HttpResponse("GrooveGuesser is a song guessing game.") (WIP PF)
+    return render(request, 'about.html')
+
+
+
+
+
+#registration app (WIP PF)
+# def registerview(request):
+#     form = UserCreationForm()
+#     return render(request, "users/registers.html", { "form": form })
