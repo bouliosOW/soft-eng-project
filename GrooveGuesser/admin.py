bb4be292972b996fe861975b1b4fd9137fca4df4
amorris0329 <andersonjmorris@icloud.com>
2025-01-29 13:52:58 -0500

    Added a basic model to be updated later for user accounts. Can connect with login/sign up page. Also included comments about the issues that may come up with the Sqlite3 database we are using for this project: See \grooveguesser\models.py and \grooveguesser\admin.py for the details.

--- GrooveGuesser/admin.py
+++ GrooveGuesser/admin.py
@@ -5,7 +5,36 @@ from .models import Account
 
 class AccountAdmin(admin.ModelAdmin):
     fields = [
-        "username", "password",
+        "username", "password", "join_date",
     ]
+    list_display = ["username", "password", "join_date"]
 
 admin.site.register(Account, AccountAdmin)
+
+# For Anderson's ease of remembering:
+# admin username = admin
+# admin email = admin@mail.com
+# admin password = password
+#others can use this data when creating their own admins for simplicity, 
+#but each admin account will be separate, and will only work on our own machines
+
+'''
+    IMPORTANT!!! -------------------------------------------
+    
+    Since our file uses sqlite3, each database may or may not be specific to individual users.
+    That means that pushing the files to Github WILL NOT transfer any data we have saved in our
+    databse in our own work.
+
+    THEREFORE, we will either have to use seperate admin accounts OR find a way to pre-populate the
+    database with a shared admin account. I am looking into a way to do this, but we can discuss this in/after class
+    on Thursday.
+
+
+    ADDITIONALLY,
+
+    If you are having any troubles with the database, models, or the admin page's use of them,
+    then you may be having problems with migrations.
+    See the comment in \grooveguesser\models.py for help
+    
+'''
+
