99235d28ee21fb6e765ae3c358e1ece248877503
amorris0329 <andersonjmorris@icloud.com>
2025-02-04 19:52:22 -0500

    I've added a base template that will apply styling to every template we have. I will soon add a navbar. Thankfully we have Bootstrap attached, so that will be easy. Any changes made to style.css will apply to every page, however, so you can

--- user/models.py
+++ user/models.py
@@ -28,3 +28,32 @@ class Leaderboard(models.Model):
 
     Without both, no changes made will work
  '''
+
+'''
+    IMPORTANT!!! -------------------------------------------
+    
+    Since our file uses sqlite3, each database may or may not be specific to individual users.
+    That means that pushing the files to Github WILL NOT transfer any data we have saved in our
+    databse in our own work.
+
+    THEREFORE, we will either have to use seperate admin accounts OR find a way to pre-populate the
+    database with a shared admin account. I am looking into a way to do this, but we can discuss this in/after class
+    on Thursday.
+
+    NOTICE that this problem will apply to all information stored in the database. Anything not
+    preset will not be saved, even with migrations. 
+
+    TO ENSURE ALL MIGRATIONS ARE ADDED, USE:
+        python manage.py migrate
+    AFTER EVERY PULL TO ENSURE ALL OF OUR DATABASES WILL HAVE THE SAME TABLES, FIELDS, ETC.
+    THIS WILL NOT POPULATE TABLES WITH DATA, BUT IT WILL ENSURE 
+    THAT ALL TABLES ARE SAME FOR ALL PARTNERS IN THIS PROJECT
+
+
+    ADDITIONALLY,
+
+    If you are having any troubles with the database, models, or the admin page's use of them,
+    then you may be having problems with migrations.
+    See the comment in \grooveguesser\models.py for help
+
+'''
