73d89539df1ff89e45cc81e3e8a390f262b52526
PatrickFratus <fratusp52@gmail.com>
2025-02-06 17:30:31 -0500

    image logo and admin fix

--- myproject/urls.py
+++ myproject/urls.py
@@ -24,6 +24,8 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path,include
+from django.conf.urls.static  import static #media imports
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -32,3 +34,5 @@ urlpatterns = [
 
     path('user/', include('user.urls')) #for user urls
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
