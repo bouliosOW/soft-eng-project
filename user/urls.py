a7594282ad6d143eff8f161e3f70f1d76cf3a7b7
amorris0329 <andersonjmorris@icloud.com>
2025-02-06 16:25:33 -0500

    Some css changes, not done with that. Mostly trying and failing to get the login form to work

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,11 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path('', views.users),
-    path("login", views.login, name="user.login")
+    path("login", views.login, name="user.login"),
+    # path('about', include('grooveguesser.urls')),
+    # This is causing errors, some kind of infinite loop relating to the routes?
+    # it may be difficult to have the navbar work 2 ways, will focus on forms first
 
 ]
--- user/views.py
+++ user/views.py
@@ -3,12 +3,36 @@ import os
 from django.shortcuts import render
 import requests
 import json
-
+from .forms import SignUpForm
+from django.http import HttpResponseRedirect
+from . import forms
 
 
 
 def users(request):
-    return render(request, 'user/users.html')
+    template_data = {}
+    template_data['title'] = 'Sign Up'
+    return render(request, 'user/users.html', {
+        'template_data': template_data
+    })
 
 def login(request):
-    return render(request, 'user/login.html')
+    template_data = {}
+    template_data['title'] = 'Log In'
+    form = forms.SignUpForm()
+    return render(request, 'user/login.html', { form: form }, {
+        'template_data': template_data
+    })
+
+def get_new_user(request):
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+
+        if form.is_valid():
+            # UPDATE DATABASE HERE
+            return HttpResponseRedirect('login')
+    
+    else:
+        form = SignUpForm()
+    
+    return render(request, 'users.html', {'form': form})
