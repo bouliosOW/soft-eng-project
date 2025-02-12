eccdec5774f56d0319d79a859038b38c8859b789
adam <adam.boulos@gmail.com>
2025-02-11 19:45:50 -0500

    Got working video playing on game

--- GrooveGuesser/views.py
+++ GrooveGuesser/views.py
@@ -4,6 +4,9 @@ from django.http import HttpResponse,JsonResponse
 import random
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
+from django.conf import settings
+from googleapiclient.discovery import build
+from django.shortcuts import render
 
 #grooveguesser test responses
 def index(request):
@@ -29,12 +32,23 @@ def game(request):
         'template_data': template_data
     })
 
+def get_video_details(video_id):
 
+    youtube = build("youtube", "v3", developerKey=settings.YOUTUBE_API_KEY)
     
+    request = youtube.videos().list(
+        part="snippet",
+        id=video_id
+    )
+    
+    return None
 
+def game(request):
+    """View function to display a YouTube video in the game page."""
+    
+    video_id = "3JZ_D3ELwOQ" 
+    video_details = get_video_details(video_id)
+    
+    print("Video Details:", video_details)
 
-
-#registration app (WIP PF)
-# def registerview(request):
-#     form = UserCreationForm()
-#     return render(request, "users/registers.html", { "form": form })
+    return render(request, 'game.html', {"video_id": video_id, "video_details": video_details})
