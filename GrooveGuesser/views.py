92ccb8c985e7b25a55e221130b7d89d10defdfa2
colsen hernandez <colsenhernandez@gmail.com>
2025-02-19 13:15:18 -0500

    removed spotipy import that did nothing

--- GrooveGuesser/views.py
+++ GrooveGuesser/views.py
@@ -2,8 +2,7 @@ from django.shortcuts import render
 # from django.contrib.auth.forms import UserCreationForm #registration (WIP PF)
 from django.http import HttpResponse,JsonResponse
 import random
-import spotipy
-from spotipy.oauth2 import SpotifyClientCredentials
+
 from django.conf import settings
 from googleapiclient.discovery import build
 from django.shortcuts import render
