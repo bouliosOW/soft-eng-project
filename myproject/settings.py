688c4885cae7b5fdeae0cc407c56c4441061fb27
Matt Flammenspeck <mflammenspeck@gmail.com>
2025-02-20 16:19:28 -0500

    wip updated styleing

--- myproject/settings.py
+++ myproject/settings.py
@@ -119,9 +119,8 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static') #for the static files for website design
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
