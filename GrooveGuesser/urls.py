821bb298ecf7fa40dabaaf13a5443c1c4734696c
adam <adam.boulos@gmail.com>
2025-03-25 20:50:25 -0400

    trying to merge Websites and Game

--- GrooveGuesser/urls.py
+++ GrooveGuesser/urls.py
@@ -7,13 +7,10 @@ urlpatterns = [
     path('about', views.about, name="about"),
     path('game/', views.game, name='game'),
     path('user/', include('user.urls')),
-<<<<<<< HEAD
     path('audio-player/', views.audio_player, name='audio-player'),
-    path('leaderboard', views.leaderboard, name="leaderboard")
-
-=======
     path('leaderboard', views.leaderboard, name="leaderboard"),
->>>>>>> feature/Websites
+
+    path('leaderboard', views.leaderboard, name="leaderboard"),
     #user registration app
     # path('register',views.registerview, name="register"),
 
